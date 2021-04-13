2a74afb641c887284abbd553079cbd1497b9e1f5
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-04-12 18:02:49 -0700

    Lab06

--- js/app.js
+++ js/app.js
@@ -1,3 +1,59 @@
 'use strict';
 
-alert ("I am alive!");
+alert ('I am alive!');
+
+//What do we need to know
+//Location
+//How many customer
+//How many cookies sold
+
+// const shop = {
+// local: '',
+// customers: 0,
+// cookies: 0,
+// getLocation: function() {
+//   console.log(this.local);
+//   let cityList =['Seattle','Tokoyo','Dubai','Paris','Lima'];
+// }
+// }
+
+
+const locarray = [locTokyo, locSeattle, locLima, locDubai,locParis];
+const times = ['6a','7a','8a','9a','10a','11a','12p','1p','2p','3p','4p','5p','6p','7p',];
+
+
+const locTokyo = {
+  name: Tokyo,
+  mincust: 3,
+  maxcust: 24,
+  cph: 1.2,
+
+};
+
+const locSeattle = {
+  name: Seattle,
+  mincust: 23,
+  maxcust: 65,
+  cph: 6.3,
+};
+
+const locLima = {
+  name: Lima,
+  mincust: 2,
+  maxcust: 16,
+  cph: 4.6,
+};
+
+const locDubai = {
+  name: Dubai,
+  mincust: 11,
+  maxcust: 38,
+  cph: 3.7,
+};
+
+const locParis = {
+  name: Paris,
+  mincust: 20,
+  maxcust: 38,
+  cph: 2.3,
+};
